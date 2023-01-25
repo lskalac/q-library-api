eec4831df3a38f02e0bffdd5a2c598d976706bf5
Lorena Škalac <lorena.skalac@softwaresauna.com>
2023-01-25 17:11:44 +0100

    - setup connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,27 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ConfigService} from '@nestjs/config';
+import { async } from 'rxjs';
+import { entites } from './typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async( configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          logging: true,
+          entities: entites,
+          host: configService.get('DATABASE_HOST'),
+          port: configService.get('DATABASE_PORT'),
+          username: configService.get('DATABASE_USER'),
+          database: configService.get('DATABASE_NAME'),
+          password: configService.get('DATABASE_PASSWORD'),
+        }
+      }
+    })
+  ],
   controllers: [],
   providers: [],
 })
