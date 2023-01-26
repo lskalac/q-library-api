92aaa6a26cf1195d41a8d7f62c082c8b5ebb204a
Lorena Škalac <lorena.skalac@gmail.com>
2023-01-26 23:44:12 +0100

    - added isActive column to user table

---
+++ src/typeorm/migrations/1674772879283-UserIsActiveColumn.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm"
+
+export class UserIsActiveColumn1674772879283 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('user', new TableColumn({
+            name: 'isActive',
+            type: 'boolean',
+            isNullable: false,
+            default: true
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('user', 'isActive');
+    }
+
+}
