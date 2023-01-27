18fc9e13a85562466d7720f8c5eb7eea6496aec8
Lorena Škalac <lorena.skalac@gmail.com>
2023-01-27 21:45:10 +0100

    - swagger description

--- src/controllers/books/books.controller.ts
+++ src/controllers/books/books.controller.ts
@@ -17,6 +17,7 @@ import {
 	ApiOkResponse,
 	ApiCreatedResponse,
 	ApiNotFoundResponse,
+	ApiForbiddenResponse,
 } from '@nestjs/swagger';
 import {BookDto} from 'src/dtos/books/Book.dto';
 import {CreateBookDto} from 'src/dtos/books/CreateBook.dto';
@@ -43,6 +44,7 @@ export class BooksController {
 
 	@Get(':id')
 	@ApiOkResponse({type: BookDto, description: 'Requested book'})
+	@ApiForbiddenResponse({description: 'User does not have permission for this action'})
 	async getById(@Param('id') id: string, @Request() req): Promise<BookDto> {
 		const book = await this.bookService.getById(id);
 		await this.validateBookOwnership(req.user, book.authorId);
@@ -54,6 +56,7 @@ export class BooksController {
 		type: BookDto,
 		description: 'Book successfully created',
 	})
+	@ApiForbiddenResponse({description: 'User does not have permission for this action'})
 	async create(
 		@Body() book: CreateBookDto,
 		@Request() req
@@ -66,6 +69,7 @@ export class BooksController {
 	@Put(':id')
 	@ApiOkResponse({description: 'Book sucessfully updated'})
 	@ApiNotFoundResponse({description: 'Requested book not found'})
+	@ApiForbiddenResponse({description: 'User does not have permission for this action'})
 	async update(
 		@Param('id') id: string,
 		@Body() book: CreateBookDto,
@@ -82,6 +86,7 @@ export class BooksController {
 	@Delete(':id')
 	@ApiOkResponse({description: 'Book sucessfully deleted'})
 	@ApiNotFoundResponse({description: 'Requested book not found'})
+	@ApiForbiddenResponse({description: 'User does not have permission for this action'})
 	async delete(@Param('id') id: string, @Request() req): Promise<void> {
 		await this.validateExistanceAndOwnership(id, req.user);
 
